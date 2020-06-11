82f50d8bb64a4a05bb008b5b4ef9b0ab1cd01bd7
Jissy-Joy <66547360+Jissy-Joy@users.noreply.github.com>
2020-06-11 15:51:23 +0530

    Datastructures
    Assigning elements to lists
    Accessing elements from a tuple 
    Deleting elements from a dictionary

---
+++ datastructures.py
@@ -0,0 +1,24 @@
+#assignjng elements to lists 
+
+list1=[5,10,15]
+list2=["apple", "peaches","orange"]
+print(list1) 
+list1.append(2) 
+print(list1)
+print(list2) 
+list2.append("mango") 
+print(list2)
+
+#accessing elements from a tuple
+
+tuple1=(5, 10,15,20) 
+print(tuple1) 
+print(tuple1[2])
+print(tuple1[1: ])
+
+#deleting element from a dictionary 
+
+dict1={"albin":10,"salvin":20,"johnvin":30}
+print(dict1) 
+del dict1["albin"] 
+print(dict1)
